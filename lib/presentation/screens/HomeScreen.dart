95b4a93db0459b6c109f8ae7e1d46b8ab158bc2c
sabbirafmkhan <sabbirafmkhan@gmail.com>
2025-03-03 21:54:29 +0600

    create card

--- lib/presentation/screens/HomeScreen.dart
+++ lib/presentation/screens/HomeScreen.dart
@@ -10,6 +10,47 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Products",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.lightGreen,
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Card(
+            elevation: 4,
+            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: ListTile(
+              leading: Icon(Icons.image, size: 50),
+              title: Text(
+                "Iphone 16 pro",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text("Price: \$999 | Qty: 20"),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
+                  SizedBox(width: 10),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.delete, color: Colors.red),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
