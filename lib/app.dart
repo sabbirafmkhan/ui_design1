43fd02d0b041d5717c598843e3513442664326f8
sabbirafmkhan <sabbirafmkhan@gmail.com>
2025-03-03 21:17:03 +0600

    Setup project

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:ui_design1/presentation/screens/HomeScreen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
